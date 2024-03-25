36dbe2c784175b6059afc4496f97941ac86e281f
henryssims <134118548+henryssims@users.noreply.github.com>
2024-03-24 21:24:53 -0400

    changed name of auth.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "./api/auth/[...nextauth]/route";
+import { authOptions } from "./api/auth/[...nextauth]/auth";
 import { User } from "./user";
 
 export default async function Page() {
